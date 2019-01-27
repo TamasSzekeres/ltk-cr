eb1ff47d91a12f389dd53dfc1ff7765d7b276c56
Wesley Moxam <wes@4ormat.com>
2019-01-27 01:18:21 -0500

    Refactor line edit painter

--- src/ltk/widget_painters/line_edit_painter.cr
+++ src/ltk/widget_painters/line_edit_painter.cr
@@ -3,19 +3,29 @@ module Ltk
     getter line_edit, painter
 
     delegate ctx, fill_round_rect, font_extents, to: painter
+    delegate focused?, height, text, text_translate, width, to: line_edit
 
     def initialize(@line_edit : LineEdit, @painter : Painter)
     end
 
     def draw
-      focused = line_edit.focused?
-      w = line_edit.width
-      h = line_edit.height
+      draw_borders!
+      clip_region!
+      draw_text!
+      draw_cursor!
+    end
 
-      #ctx.scale 3.0_f64, 3.0_f64
+    # Clip region for text and cursor
+    private def clip_region!
+      ctx.rectangle(5.0_f64, 0_f64, width - 5.0_f64, height.to_f64)
+      ctx.clip
+    end
 
-      # Draw Borders
-      if focused
+    private def draw_borders!
+      w = width
+      h = height
+
+      if focused?
         ctx.set_source_rgb 83.0_f64 / 255.0_f64, 160.0_f64 / 255.0_f64, 237.0_f64 / 255.0_f64
       else
         pattern = Pattern.create_linear 0.0_f64, 0.0_f64, 0.0_f64, h.to_f
@@ -26,7 +36,7 @@ module Ltk
       fill_round_rect 0, 0, w, h, 3
 
       w -= 2; h -= 2
-      if focused
+      if focused?
         ctx.set_source_rgb 48.0_f64 / 255.0_f64, 60.0_f64 / 255.0_f64, 72.0_f64 / 255.0_f64
       else
         pattern = Pattern.create_linear 0.0_f64, 0.0_f64, 0.0_f64, h.to_f
@@ -39,27 +49,9 @@ module Ltk
       w -= 2; h -= 2
       ctx.set_source_rgb 56.0_f64 / 255.0_f64, 56.0_f64 / 255.0_f64, 56.0_f64 / 255.0_f64
       fill_round_rect 2, 2, w, h, 3
+    end
 
-      # Clip region for text and cursor
-      w = line_edit.width
-      h = line_edit.height
-      ctx.rectangle(5.0_f64, 0_f64, w - 5.0_f64, h.to_f64)
-      ctx.clip
-
-      # Draw Text
-      text = line_edit.text
-      text_translate = line_edit.text_translate
-
-      extents = ctx.text_extents text
-      x = 5.0_f64 + text_translate.x
-      y = (line_edit.height / 2.0_f64) - (font_extents.height / 2 - font_extents.ascent) + text_translate.y
-
-      ctx.move_to x, y
-
-      ctx.set_source_rgb 0.85_f64, 0.85_f64, 0.85_f64
-      ctx.show_text text
-
-      # Draw Cursor
+    def draw_cursor!
       if line_edit.cursor_visible? && line_edit.cursor_position >= 0
         extents = ctx.text_extents text[0...line_edit.cursor_position]
         x = 5.5_f64 + extents.width
@@ -69,11 +61,22 @@ module Ltk
           puts "rect.width > 0 : #{rect.width}"
         else
           ctx.move_to rect.x.to_f64, 4.0_f64
-          ctx.line_to rect.x.to_f64, h.to_f64
+          ctx.line_to rect.x.to_f64, height.to_f64
           ctx.line_width = 1.0
           ctx.stroke
         end
       end
+    end
+
+    def draw_text!
+      extents = ctx.text_extents text
+      x = 5.0_f64 + text_translate.x
+      y = (line_edit.height / 2.0_f64) - (font_extents.height / 2 - font_extents.ascent) + text_translate.y
+
+      ctx.move_to x, y
+
+      ctx.set_source_rgb 0.85_f64, 0.85_f64, 0.85_f64
+      ctx.show_text text
     end
   end
 end
