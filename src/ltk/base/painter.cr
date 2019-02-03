6e0313d400a52ef4ce76f15e8cb2d052a522af46
TamasSzekeres <szektam2@gmail.com>
2019-02-03 21:27:16 +0100

    mod: Painter enhancements

--- src/ltk/base/painter.cr
+++ src/ltk/base/painter.cr
@@ -8,8 +8,8 @@ module Ltk
   include Cairo
 
   struct Painter
-    getter brush : Brush = Color::WHITE.as(Brush)
     getter font_extents : Cairo::FontExtents
+    getter brush : Brush = Color::WHITE.as(Brush)
     getter pen : Pen = Pen.new
     private getter space_width : Float64
 
@@ -31,7 +31,8 @@ module Ltk
 
       @space_width = @ctx.text_extents("a a").width - @ctx.text_extents("aa").width
 
-      apply_pen(@pen)
+      apply @brush
+      apply @pen
     end
 
     # def finalize
@@ -41,13 +42,143 @@ module Ltk
       # Cairo.pattern_destroy @pattern unless @pattern == nil
     # end
 
-    def draw_line(x0 : Int32, y0 : Int32, x1 : Int32, y1 : Int32)
-      # puts "draw line r=#{@pen.color.red} w=#{@pen.width}"
-      # puts "ctx.line_width = #{@ctx.line_width}"
+    def clear
+      @ctx.save
+      @ctx.set_source_rgb 1.0, 1.0, 1.0
+      @ctx.paint
+      @ctx.restore
+    end
 
+    def stroke(pen : Pen, &block)
+      with_source pen do
+        block.call
+        @ctx.stroke
+      end
+    end
+
+    def fill(brush : Brush, &block)
+      with_source brush do
+        block.call
+        @ctx.fill
+      end
+    end
+
+    def arc(center_x : Float64, center_y : Float64, radius : Float64, start_angle : Float64, end_angle : Float64)
+      @ctx.arc(center_x, center_y, radius, start_angle, end_angle)
+    end
+
+    def arc(center : PointF, radius : Float64, start_angle : Float64, end_angle : Float64)
+      @ctx.arc(center.x, center.y, radius, start_angle, end_angle)
+    end
+
+    def arc(x : Float64, y : Float64, width : Float64, height : Float64, start_angle : Float64, end_angle : Float64)
+      @ctx.save
+      # TODO: implement
+      @ctx.restore
+    end
+
+    def circle(center_x : Int32, center_y : Int32, radius : Int32)
+      @ctx.arc(center_x.to_f, center_y.to_f radius.to_f, 0.0, Math::PI * 2.0)
+    end
+
+    def circle(center : Point, radius : Int32)
+      @ctx.arc(center.x, center.y, radius.to_f, 0.0, Math::PI * 2.0)
+    end
+
+    def circle(center_x : Float64, center_y : Float64, radius : Float64)
+      @ctx.arc(center_x, center_y, radius, 0.0, Math::PI * 2.0)
+    end
+
+    def circle(center : PointF, radius : Float64)
+      @ctx.arc(center.x, center.y, radius, 0.0, Math::PI * 2.0)
+    end
+
+    def ellipse(x : Float64, y : Float64, width : Float64, height : Float64)
+      sx, sy = 1.0, 1.0
+      r = Math.min(width, height) / 2.0
+      if width > height
+        sx = width / height
+      else
+        sy = height / width
+      end
+
+      @ctx.save
+      @ctx.translate(x, y)
+      @ctx.scale(sx, sy)
+      @ctx.arc(r, r, r, 0.0, Math::PI * 2.0)
+      @ctx.restore
+    end
+
+    def ellipse(x : Int32, y : Int32, width : Int32, height : Int32)
+      ellipse(x.to_f, y.to_f, width.to_f, height.to_f)
+    end
+
+    def ellipse(rect : Rect)
+      ellipse(rect.x, rect.y, rect.width, rect.height)
+    end
+
+    def ellipse(rect : RectF)
+      ellipse(rect.x, rect.y, rect.width, rect.height)
+    end
+
+    def polygon(points : Array(PointF))
+      points.each_with_index do |p, i|
+        if i == 0
+          @ctx.move_to(p.x, p.y)
+        else
+          @ctx.line_to(p.x, p.y)
+        end
+      end
+      p = points[0]
+      @ctx.line_to(p.x, p.y)
+    end
+
+    def rectangle(x : Int32, y : Int32, width : Int32, height : Int32)
+      @ctx.rectangle(x.to_f, y.to_f, width.to_f, height.to_f)
+    end
+
+    def rectangle(rect : Rect)
+      @ctx.rectangle(rect.x.to_f, rect.y.to_f, rect.width.to_f, rect.height.to_f)
+    end
+
+    def rectangle(x : Float64, y : Float64, width : Float64, height : Float64)
+      @ctx.rectangle(x, y, width, height)
+    end
+
+    def rectangle(rect : RectF)
+      @ctx.rectangle(rect.x, rect.y, rect.width, rect.height)
+    end
+
+    def line(x0 : Int32, y0 : Int32, x1 : Int32, y1 : Int32)
       @ctx.move_to(x0.to_f, y0.to_f)
       @ctx.line_to(x1.to_f, y1.to_f)
-      @ctx.stroke
+    end
+
+    def path
+      @ctx.new_sub_path
+      yield
+      @ctx.close_path
+    end
+
+    def translate(tx : Float64, ty : Float64)
+      @ctx.save
+      @ctx.translate(tx, ty)
+      yield
+      @ctx.restore
+    end
+
+    def scale(sx : Float64, sy : Float64)
+      @ctx.save
+      @ctx.scale(sx, sy)
+      yield
+      @ctx.restore
+    end
+
+    def rotate(angle : Float64)
+      @ctx.save
+      @ctx.rotate(angle)
+      yield
+      @ctx.restore
     end
 
     def draw_round_rect(x : Int32, y : Int32, w : Int32, h : Int32, r : Int32)
@@ -90,47 +221,83 @@ module Ltk
     end
 
     def pen=(@pen : Pen)
-      apply_pen(@pen)
+      apply @pen
     end
 
     def pen=(color : Color)
       @pen = Pen.new(color)
-      apply_pen(@pen)
+      apply @pen
     end
 
-    private def apply_brush
-      # unless @pattern.is_a? Nil
-      #   Cairo.pattern_destroy @pattern
-      #   @pattern = nil
-      # end
-      #
-      # case @brush
-      # when Color
-      #   col = brush.as Color
-      #   mul = 1.0_f64 / 255.0_f64
-      #   Cairo.set_source_rgba(@ctx,
-      #     col.red * mul, col.green * mul, col.blue * mul, col.alpha * mul)
-      # when LinearGradient
-      #   lg = @brush.as LinearGradient
-      #   s = lg.start
-      #   e = lg.end
-      #   @pattern = Cairo.pattern_create_linear(s.x, s.y, e.x, e.y)
-      #
-      #   lg.stops.each do |stop|
-      #     Cairo.pattern_add_color_stop_rgba(
-      #       @pattern, stop.offset, stop.red,
-      #       stop.green, stop.blue, stop.alpha)
-      #   end
-      #   Cairo.set_source @ctx, @pattern
-      # else
-      # end
+    private def apply(brush : Brush)
+      case brush
+      when Color
+        col = brush.as Color
+        mul = 1.0_f64 / 255.0_f64
+        @ctx.set_source_rgba(col.red * mul, col.green * mul, col.blue * mul, col.alpha * mul)
+      when LinearGradient
+        lg = brush.as LinearGradient
+        s = lg.start
+        e = lg.end
+        pattern = Pattern.create_linear(s.x, s.y, e.x, e.y)
+        lg.stops.each do |stop|
+          pattern.add_color_stop(stop.offset, stop.red, stop.green, stop.blue, stop.alpha)
+        end
+        @ctx.source = pattern
+      else
+        raise "Unknown brush type: #{brush.class}"
+      end
     end
 
-    private def apply_pen(pen : Pen)
+    private def apply(pen : Pen)
       color = pen.color
       mul = 1.0_f64 / 255.0_f64
       @ctx.set_source_rgba(color.red * mul, color.green * mul, color.blue * mul, color.alpha * mul)
       @ctx.line_width = pen.width
+    end
+
+    private def with_source(brush : Brush)
+      @ctx.save
+      begin
+        case brush
+        when Color
+          col = brush.as Color
+          mul = 1.0_f64 / 255.0_f64
+          @ctx.set_source_rgba(col.red * mul, col.green * mul, col.blue * mul, col.alpha * mul)
+          yield
+        when LinearGradient
+          lg = @brush.as LinearGradient
+          s = lg.start
+          e = lg.end
+          pattern = Pattern.create_linear(s.x, s.y, e.x, e.y)
+          begin
+            lg.stops.each do |stop|
+              pattern.add_color_stop(stop.offset, stop.red, stop.green, stop.blue, stop.alpha)
+            end
+            @ctx.source = pattern
+            yield
+          ensure
+            pattern.finalize
+          end
+        else
+          raise "Unknown brush type: #{brush.class}"
+        end
+      ensure
+        @ctx.restore
+      end
+    end
+
+    private def with_source(pen : Pen)
+      @ctx.save
+      begin
+        color = pen.color
+        mul = 1.0_f64 / 255.0_f64
+        @ctx.set_source_rgba(color.red * mul, color.green * mul, color.blue * mul, color.alpha * mul)
+        @ctx.line_width = pen.width
+        yield
+      ensure
+        @ctx.restore
+      end
     end
 
     def text_extents(text : String, trailing_ws : Bool = true) : Cairo::TextExtents
