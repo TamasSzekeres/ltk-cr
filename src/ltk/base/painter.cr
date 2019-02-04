c9c8dbc06e71ede5971bcf426ec0544ef12dfbc8
Szekeres Tamás <szekeres.tamas@wwdh.hu>
2019-02-04 16:46:39 +0100

    add: Painter functions

--- src/ltk/base/painter.cr
+++ src/ltk/base/painter.cr
@@ -9,8 +9,6 @@ module Ltk
 
   struct Painter
     getter font_extents : Cairo::FontExtents
-    getter brush : Brush = Color::WHITE.as(Brush)
-    getter pen : Pen = Pen.new
     private getter space_width : Float64
 
     def initialize(widget : Widget)
@@ -20,7 +18,6 @@ module Ltk
         widget.width, widget.height
       )
       @ctx = Cairo::Context.new @surface
-      # @pattern = nil
 
       @ctx.select_font_face("Sans",
         Cairo::FontSlant::Normal,
@@ -30,17 +27,7 @@ module Ltk
       @font_extents = @ctx.font_extents
 
       @space_width = @ctx.text_extents("a a").width - @ctx.text_extents("aa").width
-
-      apply @brush
-      apply @pen
     end
-
-    # def finalize
-      # Cairo.destroy @ctx
-      # Cairo.surface_destroy @surface
-      #
-      # Cairo.pattern_destroy @pattern unless @pattern == nil
-    # end
 
     def clear
       @ctx.save
@@ -49,17 +36,50 @@ module Ltk
       @ctx.restore
     end
 
-    def stroke(pen : Pen, &block)
-      with_source pen do
-        block.call
+    def draw(pen : Pen)
+      @ctx.save
+      begin
+        color = pen.color
+        mul = 1.0_f64 / 255.0_f64
+        @ctx.set_source_rgba(color.red * mul, color.green * mul, color.blue * mul, color.alpha * mul)
+        @ctx.line_width = pen.width
+        yield self
         @ctx.stroke
+      ensure
+        @ctx.restore
       end
     end
 
-    def fill(brush : Brush, &block)
-      with_source brush do
-        block.call
-        @ctx.fill
+    def fill(brush : Brush)
+      @ctx.save
+      begin
+        case brush
+        when Color
+          col = brush.as Color
+          mul = 1.0_f64 / 255.0_f64
+          @ctx.set_source_rgba(col.red * mul, col.green * mul, col.blue * mul, col.alpha * mul)
+          yield self
+          @ctx.fill
+        when LinearGradient
+          lg = @brush.as LinearGradient
+          s = lg.start
+          e = lg.end
+          pattern = Pattern.create_linear(s.x, s.y, e.x, e.y)
+          begin
+            lg.stops.each do |stop|
+              pattern.add_color_stop(stop.offset, stop.red, stop.green, stop.blue, stop.alpha)
+            end
+            @ctx.source = pattern
+            yield self
+            @ctx.fill
+          ensure
+            pattern.finalize
+          end
+        else
+          raise "Unknown brush type: #{brush.class}"
+        end
+      ensure
+        @ctx.restore
       end
     end
 
@@ -72,8 +92,18 @@ module Ltk
     end
 
     def arc(x : Float64, y : Float64, width : Float64, height : Float64, start_angle : Float64, end_angle : Float64)
+      sx, sy = 1.0, 1.0
+      r = Math.min(width, height) / 2.0
+      if width > height
+        sx = width / height
+      else
+        sy = height / width
+      end
+
       @ctx.save
-      # TODO: implement
+      @ctx.translate(x, y)
+      @ctx.scale(sx, sy)
+      @ctx.arc(r, r, r, start_angle, end_angle)
       @ctx.restore
     end
 
@@ -121,6 +151,26 @@ module Ltk
       ellipse(rect.x, rect.y, rect.width, rect.height)
     end
 
+    def line(x0 : Float64, y0 : Float64, x1 : Float64, y1 : Float64)
+      @ctx.move_to(x0, y0)
+      @ctx.line_to(x1, y1)
+    end
+
+    def line(p1 : PointF, p2 : PointF)
+      @ctx.move_to(p1.x, p1.y)
+      @ctx.line_to(p2.x, p2.y)
+    end
+    
+    def line(x0 : Int32, y0 : Int32, x1 : Int32, y1 : Int32)
+      @ctx.move_to(x0.to_f, y0.to_f)
+      @ctx.line_to(x1.to_f, y1.to_f)
+    end
+
+    def line(p1 : Point, p2 : Point)
+      @ctx.move_to(p1.x.to_f, p1.y.to_f)
+      @ctx.line_to(p2.x.to_f, p2.y.to_f)
+    end
+
     def point(x : Float64, y : Float64)
       rectangle(x, y, 1.0, 1.0)
     end
@@ -138,6 +188,16 @@ module Ltk
     end
 
     def polygon(points : Array(PointF))
+      polyline(points)
+      p = points[0]
+      @ctx.line_to(p.x, p.y)
+    end
+
+    def polygon(poly : PolygonF)
+      polygon(poly.points)
+    end
+
+    def polyline(points : Array(PointF))
       points.each_with_index do |p, i|
         if i == 0
           @ctx.move_to(p.x, p.y)
@@ -145,8 +205,10 @@ module Ltk
           @ctx.line_to(p.x, p.y)
         end
       end
-      p = points[0]
-      @ctx.line_to(p.x, p.y)
+    end
+
+    def polyline(poly : PolygonF)
+      polyline(poly.points)
     end
 
     def rectangle(x : Int32, y : Int32, width : Int32, height : Int32)
@@ -165,53 +227,92 @@ module Ltk
       @ctx.rectangle(rect.x, rect.y, rect.width, rect.height)
     end
 
-    def line(x0 : Int32, y0 : Int32, x1 : Int32, y1 : Int32)
-      @ctx.move_to(x0.to_f, y0.to_f)
-      @ctx.line_to(x1.to_f, y1.to_f)
+    def rounded_rectangle(x : Float64, y : Float64, width : Float64, height : Float64, radius : Float64)
+      half_width = width / 2.0
+      half_height = height / 2.0
+
+      if radius > half_width
+        radius = half_width
+      end
+      if radius > half_height
+        radius = half_height
+      end
+
+      degrees = Math::PI / 180.0_f64
+
+      @ctx.new_sub_path
+      @ctx.arc(x + width - radius, y + radius,          radius, -90 * degrees,   0 * degrees)
+      @ctx.arc(x + width - radius, y + height - radius, radius,   0 * degrees,  90 * degrees)
+      @ctx.arc(x + radius,         y + height - radius, radius,  90 * degrees, 180 * degrees)
+      @ctx.arc(x + radius,         y + radius,          radius, 180 * degrees, 270 * degrees)
+      @ctx.close_path
+    end
+
+    def rounded_rectangle(rect : RectF, radius : Float64)
+      rounded_rectangle(rect.x, rect.y, rect.width, rect.height, radius)
+    end
+
+    def rounded_rectangle(x : Int32, y : Int32, width : Int32, height : Int32, radius : Int32)
+      rounded_rectangle(x.to_f, y.to_f, width.to_f, height.to_f, radius.to_f)
+    end
+
+    def rounded_rectangle(rect : Rect, radius : Int32)
+      rounded_rectangle(rect.x.to_f, rect.y.to_f, rect.width.to_f, rect.height.to_f, radius.to_f)
+    end
+
+    def rounded_rectangle(x : Float64, y : Float64, width : Float64, height : Float64, radius_x : Float64, radius_y : Float64)
+      sx, sy = 1.0, 1.0
+      r = Math.min(radius_x, radius_y)
+      if radius_x > radius_y
+        sx = radius_x / radius_y
+      else
+        sy = radius_y / radius_x
+      end
+
+      @ctx.save
+      @ctx.translate(x, y)
+      @ctx.scale(sx, sy)
+      rounded_rectangle(0.0, 0.0, width / sx, height / sy, r)
+      @ctx.restore
+    end
+
+    def rounded_rectangle(rect : RectF, radius_x : Float64, radius_y : Float64)
+      rounded_rectangle(rect.x, rect.y, rect.width, rect.height, radius_x, radius_y)
+    end
+
+    def rounded_rectangle(x : Int32, y : Int32, width : Int32, height : Int32, radius_x : Int32, radius_y : Int32)
+      rounded_rectangle(x.to_f, y.to_f, width.to_f, height.to_f, radius_x.to_f, radius_y.to_f)
+    end
+
+    def rounded_rectangle(rect : Rect, radius_x : Int32, radius_y : Int32)
+      rounded_rectangle(rect.x.to_f, rect.y.to_f, rect.width.to_f, rect.height.to_f, radius_x.to_f, radius_y.to_f)
     end
 
     def path
       @ctx.new_sub_path
-      yield
+      yield self
       @ctx.close_path
     end
 
     def translate(tx : Float64, ty : Float64)
       @ctx.save
       @ctx.translate(tx, ty)
-      yield
+      yield self
       @ctx.restore
     end
 
     def scale(sx : Float64, sy : Float64)
       @ctx.save
       @ctx.scale(sx, sy)
-      yield
+      yield self
       @ctx.restore
     end
 
     def rotate(angle : Float64)
       @ctx.save
       @ctx.rotate(angle)
-      yield
+      yield self
       @ctx.restore
-    end
-
-    def draw_round_rect(x : Int32, y : Int32, w : Int32, h : Int32, r : Int32)
-      # if r > (h / 2)
-      #   r = h / 2
-      # end
-      #
-      # degrees = Math::PI / 180.0_f64
-      #
-      # @ctx.new_sub_path
-      # @ctx.arc x + w - r, y + r,     r, -90 * degrees,   0 * degrees
-      # @ctx.arc x + w - r, y + h - r, r,   0 * degrees,  90 * degrees
-      # @ctx.arc x + r,     y + h - r, r,  90 * degrees, 180 * degrees
-      # @ctx.arc x + r,     y + r,     r, 180 * degrees, 270 * degrees
-      # @ctx.close_path
-      #
-      # @ctx.stroke
     end
 
     def fill_rectangle(x : Int32, y : Int32, w : Int32, h : Int32)
@@ -234,86 +335,6 @@ module Ltk
       @ctx.close_path
 
       @ctx.fill
-    end
-
-    def pen=(@pen : Pen)
-      apply @pen
-    end
-
-    def pen=(color : Color)
-      @pen = Pen.new(color)
-      apply @pen
-    end
-
-    private def apply(brush : Brush)
-      case brush
-      when Color
-        col = brush.as Color
-        mul = 1.0_f64 / 255.0_f64
-        @ctx.set_source_rgba(col.red * mul, col.green * mul, col.blue * mul, col.alpha * mul)
-      when LinearGradient
-        lg = brush.as LinearGradient
-        s = lg.start
-        e = lg.end
-        pattern = Pattern.create_linear(s.x, s.y, e.x, e.y)
-        lg.stops.each do |stop|
-          pattern.add_color_stop(stop.offset, stop.red, stop.green, stop.blue, stop.alpha)
-        end
-        @ctx.source = pattern
-      else
-        raise "Unknown brush type: #{brush.class}"
-      end
-    end
-
-    private def apply(pen : Pen)
-      color = pen.color
-      mul = 1.0_f64 / 255.0_f64
-      @ctx.set_source_rgba(color.red * mul, color.green * mul, color.blue * mul, color.alpha * mul)
-      @ctx.line_width = pen.width
-    end
-
-    private def with_source(brush : Brush)
-      @ctx.save
-      begin
-        case brush
-        when Color
-          col = brush.as Color
-          mul = 1.0_f64 / 255.0_f64
-          @ctx.set_source_rgba(col.red * mul, col.green * mul, col.blue * mul, col.alpha * mul)
-          yield
-        when LinearGradient
-          lg = @brush.as LinearGradient
-          s = lg.start
-          e = lg.end
-          pattern = Pattern.create_linear(s.x, s.y, e.x, e.y)
-          begin
-            lg.stops.each do |stop|
-              pattern.add_color_stop(stop.offset, stop.red, stop.green, stop.blue, stop.alpha)
-            end
-            @ctx.source = pattern
-            yield
-          ensure
-            pattern.finalize
-          end
-        else
-          raise "Unknown brush type: #{brush.class}"
-        end
-      ensure
-        @ctx.restore
-      end
-    end
-
-    private def with_source(pen : Pen)
-      @ctx.save
-      begin
-        color = pen.color
-        mul = 1.0_f64 / 255.0_f64
-        @ctx.set_source_rgba(color.red * mul, color.green * mul, color.blue * mul, color.alpha * mul)
-        @ctx.line_width = pen.width
-        yield
-      ensure
-        @ctx.restore
-      end
     end
 
     def text_extents(text : String, trailing_ws : Bool = true) : Cairo::TextExtents
